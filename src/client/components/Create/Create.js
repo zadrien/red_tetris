5bbee19b0bf7df43dbd57ce8ebe7ccde637e60b8
bngo42 <bngo@student.42.fr>
2019-11-28 18:05:21 +0100

    Removing useless method + adding input limitation to 25 characters

--- src/client/components/Create/Create.js
+++ src/client/components/Create/Create.js
@@ -9,10 +9,10 @@ import "./style.css";
 const Create = ({ isCreating, onSubmit, user}) => (
   <form onSubmit={(e) => onSubmit(e, user)} id="create-room-form">
     <h2 className="align-left title small mt-3">Room's name</h2>
-    <input className="width-100 p-2" type="text" id="room" name="room" placeholder="MY AWSOME ROOM"/>
+    <input className="width-100 p-2" type="text" maxLength="25" id="room" name="room" placeholder="MY AWSOME ROOM"/>
     <h2 className="align-left title small mt-3">Game mode</h2>
     <div>
-      <input type="radio" name="mode" defaultValue="classic" checked/>
+      <input type="radio" name="mode" defaultValue="classic" defaultChecked/>
       <span className="ml-1">Classic</span>
     </div>
     <div>
@@ -25,39 +25,36 @@ const Create = ({ isCreating, onSubmit, user}) => (
 )
 
 
-const handleForm = (e) => {
-  e.preventDefault();
-  var room = { }
-  console.log(e.target);
-  room["name"] = e.target.room.value || 'Unamed room'
-  room["mode"] = e.target.mode.value
-
-  console.log("room:", room)
-
-  return room
-
-}
-
 const mapStateToProps = (state) => ({
   isCreating: state.isCreating,
   user: state.user
   
 })
+
 const mapDispatchToProps = (dispatch, e) => ({
   onSubmit: (e, user) => {
-    var room = handleForm(e)
-    var data = {
-      user,
-      room
-    }
-    dispatch(isCreating())
-    dispatch(onCreation())
-    console.log(data)
-    dispatch(emitCreate(data))
+    e.preventDefault();
+
+    let roomName = e.target.room.value,
+        roomMode = e.target.mode.value;
+
+    if (roomName.length > 0) {
+      var data = {
+        user,
+        room : {
+          name: roomName,
+          mode: roomMode
+        }
+      }
+      dispatch(isCreating())
+      dispatch(onCreation())
+      console.log(data)
+      dispatch(emitCreate(data))
     
-    //dispatch(isJoining(true, r))
-    //dispatch(emitJoin(r))
-    // dispatch(onPlayers())
+      //dispatch(isJoining(true, r))
+      //dispatch(emitJoin(r))
+      // dispatch(onPlayers())
+    }
   }
 })
 export default connect(mapStateToProps, mapDispatchToProps)(Create);
